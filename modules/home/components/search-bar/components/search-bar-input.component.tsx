b733281ea1ab32c26ff9cd02ae7b7e05a12cdf4b
Yana <yanachernushevich@gmail.com>
2024-02-06 20:14:50 +0100

    added prepopulatedSearchedValue into input

--- modules/home/components/search-bar/components/search-bar-input.component.tsx
+++ modules/home/components/search-bar/components/search-bar-input.component.tsx
@@ -2,23 +2,25 @@ import { View, TextInput } from 'react-native';
 import { FC } from 'react';
 import { useRouter } from 'expo-router';
 import tw from 'twrnc';
-import { CityGeoRequestDto } from '@api/dto/city-geo.request.dto';
 import { COLORS } from '@constants/style.constants';
+import { SearchedCity } from '@model/searched-city.model';
 
 interface ISearchBarInputProps {
-	setSearchedCity: (text: CityGeoRequestDto) => void;
+	prepopulatedSearchedValue: SearchedCity | null;
 }
 
-export const SearchBarInput: FC<ISearchBarInputProps> = ({ setSearchedCity }) => {
+export const SearchBarInput: FC<ISearchBarInputProps> = ({ prepopulatedSearchedValue }) => {
 	const router = useRouter();
+
 	return (
-		<View style={tw`flex-3 rounded-xl bg-[${COLORS.gray}] h-13`}>
+		<View style={tw` flex-3 `}>
 			<TextInput
-				style={tw`flex-1 px-2.5 text-base`}
+				style={tw`h-13 px-2.5 text-base py-0 rounded-xl bg-[${COLORS.gray}]`}
 				placeholder="Chose the City"
 				onPressIn={() => router.push('/search-modal')}
 				editable={false}
 				selectTextOnFocus={false}
+				value={prepopulatedSearchedValue ? prepopulatedSearchedValue.name : ''}
 			/>
 		</View>
 	);
--- modules/home/components/search-bar/search-bar.component.tsx
+++ modules/home/components/search-bar/search-bar.component.tsx
@@ -4,19 +4,35 @@ import { SearchBarInput } from './components/search-bar-input.component';
 import { SearchBarButton } from './components/search-bar-button.component';
 import tw from 'twrnc';
 import { useRouter } from 'expo-router';
+import { useSearchHistory } from '@context/search-history.provider';
+import { SearchedCity } from '@model/searched-city.model';
 
 export const SearchBar: FC = () => {
-	//TODO: add search functionality
-	const [searchedCity, setSearchedCity] = useState({});
+	const [prepopulatedSearchedValue, setPrepopulatedSearchedValue] = useState<SearchedCity | null>(null);
+
 	const router = useRouter();
+	const { searchedHistory } = useSearchHistory();
+
+	useEffect(() => {
+		if (searchedHistory.length > 0) {
+			const fav = searchedHistory.find(item => item[1].favorite === true);
+			setPrepopulatedSearchedValue(fav ? fav[1] : null);
+		}
+	}, [searchedHistory]);
 
 	const handleSearchPress = () => {
-		router.navigate('weather');
+		router.navigate({
+			pathname: 'weather/',
+			params: {
+				latitude: prepopulatedSearchedValue?.location.latitude,
+				longitude: prepopulatedSearchedValue?.location.longitude,
+			},
+		});
 	};
 
 	return (
 		<View style={tw`flex-row justify-between items-center mt-3 gap-2`}>
-			<SearchBarInput setSearchedCity={setSearchedCity} />
+			<SearchBarInput prepopulatedSearchedValue={prepopulatedSearchedValue} />
 			<SearchBarButton handlePress={handleSearchPress} />
 		</View>
 	);
